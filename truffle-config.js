3dd21ee9fa6af22616db842e52cd2f21aac28a1a
TepNik <tepelin.nikita@gmail.com>
2020-11-24 14:27:20 +0300

    Init

---
+++ truffle-config.js
@@ -0,0 +1,59 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+require('dotenv').config();
+const {
+    ETHERSCAN_API_KEY,
+    MNEMONIC,
+    DEPLOY_GAS_LIMIT,
+    DEPLOY_GAS_PRICE,
+    INFURA_ID_PROJECT
+} = process.env;
+
+const Web3 = require("web3");
+const web3 = new Web3();
+
+module.exports = {
+    plugins: [
+        'truffle-plugin-verify'
+    ],
+
+    api_keys: {
+        etherscan: ETHERSCAN_API_KEY
+    },
+
+    networks: {
+        ropsten: {
+            provider: () => new HDWalletProvider(MNEMONIC, 'https://ropsten.infura.io/v3/' + INFURA_ID_PROJECT),
+            network_id: 3,
+            gas: DEPLOY_GAS_LIMIT,
+            confirmations: 2,
+            skipDryRun: true
+        },
+        mainnet: {
+            provider: () => new HDWalletProvider(MNEMONIC, 'https://mainnet.infura.io/v3/' + INFURA_ID_PROJECT),
+            network_id: 1,
+            gasPrice: web3.utils.toWei(DEPLOY_GAS_PRICE, 'gwei'),
+            gas: DEPLOY_GAS_LIMIT,
+            skipDryRun: false
+        },
+        kovan: {
+            provider: () => new HDWalletProvider(MNEMONIC, 'https://kovan.infura.io/v3/' + INFURA_ID_PROJECT),
+            network_id: 42,
+            confirmations: 2,
+            gas: DEPLOY_GAS_LIMIT,
+            skipDryRun: true
+        },
+    },
+
+    compilers: {
+        solc: {
+            version: "0.6.12",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 999999
+                },
+            }
+        },
+    },
+};
